9e7c3cdc5d89976a615e28091ddfdb218cee6981
BogdanZarichnyy <bogdanzarichnyy@gmail.com>
2025-08-05 19:36:24 +0300

    add config params

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', //це потрібно для деплою на гітхаб
+  basePath: '/next-js-project-test',
   reactStrictMode:true,
   env: { // тут можна перейменувати .env дані, але як  правило в цьому немає сенсу
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
